09348cd222eb55bb830001452daf5ecc6798e5cc
Casquinha2 <tmdscasquinha@gmail.com>
2023-05-10 17:25:06 +0100

    ficheiro json

--- View.py
+++ View.py
@@ -1,20 +1,13 @@
 from Controller import *
 import tkinter as tk
+from model.Cliente import *
+
+
 class View:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
         self.frame.pack()
 
-    def login(utilizador):
         try:
-            ficheiro = ler_ficheiro_json(utilizador)
-        except FileNotFoundError:
-            print("""
-            O utilizador introduzido não tem conta criada.
-                Tente novamente ou crie um novo utilizador.
-            """)
-        else:
-            nif = input("Por favor introduza o NIF: ")
-            if nif == ficheiro[1]:
-                pass
+        self.label1 =tk.Label()
