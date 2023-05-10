cd50d79545aa9e6de782d9d13b5ea5d5345e46b4
Toroyoshi <121224137+Toroyoshi@users.noreply.github.com>
2023-05-10 17:45:46 +0100

    Add login e registo

--- View.py
+++ View.py
@@ -2,6 +2,7 @@ from Controller import *
 import tkinter as tk
 from model.ClientLinkedList import*
 from model.Cliente import *
+from tkinter import messagebox
 class View:
     def __init__(self, master):
         self.master = master
@@ -11,6 +12,7 @@ class View:
             
         #Frame
         self.master.attributes('-fullscreen', True)
+        self.master.configure(bg= '#ffe76c')
         self.frame = tk.Frame(self.master, bg='#ffe76c')
         self.frame.pack()
 
@@ -48,3 +50,63 @@ class View:
         #botão de fecho do programa
         self.shutdown_button = tk.Button(self.frame, text="Sair", font=('Arial', 14), fg='white', bg='#6d7575', command= self.master.destroy)
         self.shutdown_button.pack(pady=10, ipadx=20, ipady=5)
+
+    def registar(self):
+              #Implementar a função registo()
+
+            #Criação de um usuário
+            nome = self.nome_entry.get()
+            password = self.password_entry.get()            
+            cliente = Cliente(nome, password)
+            
+            if  self.clientes.head is None:
+                self.clientes.insert_last(cliente)
+                
+                messagebox.showinfo('Sucesso!', 'Usuário registado com sucesso. Já pode fazer o login em sua conta.')                    
+
+            else: 
+                if self.clientes.head is not None:
+                    i = self.clientes.find_username(nome)
+                    
+                    if i == -1:
+                        self.clientes.insert_last(cliente)
+                        messagebox.showinfo('Sucesso!', 'Usuário registado com sucesso. Já pode fazer o login em sua conta.')
+
+                    elif self.clientes.get(i).get_nome() == cliente.get_nome():
+                        messagebox.showinfo('Usuário existente.', 'Por favor, digite um nome de usuário diferente.')
+                        print(self.clientes.size)
+                        print(self.clientes.get_first().get_nome())
+
+
+    def login(self):
+        
+            
+        if self.clientes.size <= 0:
+            messagebox.showerror('Nenhum usuário registado', 'Por favor, registe um usuário antes de fazer login.')
+
+        else:
+            if self.clientes is not None:
+                try:
+
+                    if self.clientes.size > 0:
+                        nome = self.nome_entry.get()
+                        password = self.password_entry.get()
+                        i = self.clientes.find_username(nome)
+                        print(nome)
+                        print(password) 
+                        try:
+                            if self.clientes.get(i).get_nome() == nome:
+                                if self.clientes.get(i).get_password() == password:
+                                    messagebox.showinfo('Sucesso!', 'Usuário logado com sucesso.')
+                                    print(self.clientes.get(i).get_nome())
+                                    return True
+
+                                else:
+                                    messagebox.showerror('Erro.', 'Credênciais inválidas.')
+                                              
+
+                        except Exception or AttributeError:
+                            messagebox.showerror('Erro de login', 'Usuário não encontrado.')
+
+                except AttributeError:
+                    messagebox.showerror('erro de login', 'usuário não encontrado.')
