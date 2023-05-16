82a23fb15475f31e4e6e462f5a85c2be565e2119
Toroyoshi <121224137+Toroyoshi@users.noreply.github.com>
2023-05-16 18:08:17 +0100

    add registos

--- View.py
+++ View.py
@@ -3,6 +3,9 @@ import tkinter as tk
 from model.ClientLinkedList import*
 from model.Cliente import *
 from tkinter import messagebox
+from datetime import datetime
+
+
 class View:
     def __init__(self, master):
         self.master = master
@@ -53,10 +56,40 @@ class View:
     
     
     def frame_principal(self):
+        #Frame de despesas
         self.master.withdraw()
         self.frame1 = tk.Toplevel(self.master)
+        self.frame1.attributes('-fullscreen', True)
+        self.frame1.configure(bg= '#ffe76c')
         
-    
+        #botão de shutdown ==> ok
+        self.shutdown_button2 = tk.Button(self.frame1, text="Sair", font=('Arial', 14), fg='white', bg='#6d7575', command= quit)
+        self.shutdown_button2.pack(pady=10, ipadx=20, ipady=5)
+        
+        #botão de voltar ==> ok
+        self.exit_button = tk.Button(self.frame1, text="Sair para área de trabalho", font=('Arial', 14), fg='white', bg='#6d7575', command= self.quit)
+        self.exit_button.pack(pady=10, ipadx=20, ipady=5)
+        
+        #botão de registo de despesas 
+        self.registo_despesas_button = tk.Button(self.frame1, text="Registar despesas", font=('Arial', 14), fg='white', bg='#6d7575', command= self.registo_despesas)
+        self.registo_despesas_button.pack(pady=10, ipadx=20, ipady=5)
+#        messagebox.askquestion('', 'O utilizador ainda não definiu um orçamento mensal. Deseja definir?')
+
+        #botão de detalhes
+        self.detalhes_button = tk.Button(self.frame1, text="Ver mais detalhes", font=('Arial', 14), fg='white', bg='#6d7575')
+        self.detalhes_button.pack(pady=10, ipadx=20, ipady=5)
+
+        #botão de defição de orçamento mensal
+        self.orcamento_button = tk.Button(self.frame1, text="Definir orçamento mensal", font=('Arial', 14), fg='white', bg='#6d7575')
+        self.orcamento_button.pack(pady=10, ipadx=20, ipady=5)
+
+ 
+
+        
+    def quit(self):
+        self.frame1.destroy()
+        self.master.deiconify()
+
     def registar(self):
         nome = self.nome_entry.get()
         password = self.password_entry.get()
@@ -106,6 +139,15 @@ class View:
                         else:
                             self.frame_principal()                       
     
+    def registo_despesas(self):
+#        self.data = datetime.now()
+        self.frame2 = tk.Toplevel(self.master)
+        self.frame2.configure(bg= '#ffe76c')
+        self.valor_despesas_label = tk.Label(self.frame2, text="Valor da despesa: ", font=('Arial', 14), bg='#ffe76c')
+        self.valor_despesas_label.pack()
+        self.valor_despesas_entry = tk.Entry(self.frame2, font=('Arial', 14))
+        self.valor_despesas_entry.pack(pady=5)
+#        self.formato1 = self.data.strftime("%d/%m/%Y")
 
 #[{nome:..., senha:..., nif:...}, despesas, grafico,....]
 #[{nome:..., se, despesas, grafico,...]
